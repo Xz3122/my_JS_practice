67fb95c800cb8803060b507d1bba427f0c4d18f6
王俊邦 <st10202095@gmail.com>
2025-06-10 17:41:00 +0800

    惡作劇警示窗

---
+++ script11.js
@@ -0,0 +1,21 @@
+function prank(){
+    alert("不要點擊");
+    alert("我說了不要點擊");
+    alert("你真的很不乖");
+    alert("你再怎麼點擊都沒用");
+    alert("我說的是真的");
+    alert("我是不會消失的");
+    alert("你要不要試試看");
+    alert("按個10次，會有驚喜");
+    for(let n = 1;n <=10; n++ )
+        alert(`你按了${n}次`)
+    alert("真乖，叫你按你就按")
+}
+prank()
+if (confirm("你想要退出嗎")){
+    alert("你休想退出")
+    alert("嚇你的")
+}
+else{
+    alert("你想玩但我不玩了")
+}
